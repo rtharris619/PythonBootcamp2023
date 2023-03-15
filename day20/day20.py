529a86f311f5bfcd2895edd0aea3a963cd1aa790
Ryan Harris <ryan@synthesis.co.za>
2023-03-15 15:53:08 +0200

    Day 20 / 21 in progress

--- day20/day20.py
+++ day20/day20.py
@@ -1,4 +1,6 @@
 import day20.snake as snake
+import day20.food as food
+import day20.scoreboard as scoreboard
 from turtle import Screen
 import time
 
@@ -11,6 +13,8 @@ def day20():
     screen.tracer(0)
 
     my_snake = snake.Snake()
+    my_food = food.Food()
+    my_scoreboard = scoreboard.Scoreboard()
 
     screen.listen()
     screen.onkey(my_snake.up, "Up")
@@ -24,6 +28,11 @@ def day20():
         time.sleep(0.1)
 
         my_snake.move()
+
+        # Detects a collision with the food
+        if my_snake.head.distance(my_food) < 15:
+            my_food.refresh()
+            my_scoreboard.increase_score()
 
     screen.exitonclick()
 
