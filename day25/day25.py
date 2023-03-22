89f0543142e94b63b2dd6039ded5e76a26933b8e
Ryan Harris <ryan@synthesis.co.za>
2023-03-22 16:58:51 +0200

    Day 25 in progress

--- day25/day25.py
+++ day25/day25.py
@@ -1,7 +1,8 @@
+import pandas
 
 
 def day_25():
-    read_csv_with_pandas()
+    read_squirrel_data()
 
 
 def read_csv():
@@ -17,6 +18,38 @@ def read_csv():
 
 
 def read_csv_with_pandas():
-    import pandas
     data = pandas.read_csv("./day25/weather_data.csv")
-    print(data["temp"])
+    data_dict = data.to_dict()
+    print(data_dict)
+
+    temp_list = data['temp'].to_list()
+    print(temp_list)
+
+    mean = data['temp'].mean()
+    print(mean)
+
+    monday = data[data.day == 'Monday']
+    print(monday.temp)
+
+    max_temp_row = data[data.temp == data.temp.max()]
+    print(max_temp_row)
+
+
+def create_dataframe():
+    data_dict = {
+        "students": ["Amy", "James", "Angela"],
+        "scores": [76, 56, 65]
+    }
+
+    data = pandas.DataFrame(data_dict)
+    # data.to_csv("./day25/new_data.csv")
+
+
+def read_squirrel_data():
+    data = pandas.read_csv("./day25/2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+
+    gray_count = len(data[data['Primary Fur Color'] == 'Gray'])
+    red_count = len(data[data['Primary Fur Color'] == 'Cinnamon'])
+    black_count = len(data[data['Primary Fur Color'] == 'Black'])
+
+    print(gray_count, red_count, black_count)
---
+++ readme.md
@@ -0,0 +1,2 @@
+Day 25:
+Working with CSV data and Pandas.
