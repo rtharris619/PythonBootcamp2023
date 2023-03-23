3a4aef13be64e4eb3c5f676df6074c14833c3227
Ryan Harris <ryan@synthesis.co.za>
2023-03-23 15:10:56 +0200

    Day 25 complete

--- day25/day25.py
+++ day25/day25.py
@@ -2,7 +2,7 @@ import pandas
 
 
 def day_25():
-    read_squirrel_data()
+    states_game()
 
 
 def read_csv():
@@ -53,3 +53,43 @@ def read_squirrel_data():
     black_count = len(data[data['Primary Fur Color'] == 'Black'])
 
     print(gray_count, red_count, black_count)
+
+
+def states_game():
+    import turtle
+
+    screen = turtle.Screen()
+    screen.title('U.S. States Game')
+    image = "./day25/blank_states_img.gif"
+    screen.addshape(image)
+    turtle.shape(image)
+
+    data = pandas.read_csv("./day25/50_states.csv")
+    all_states = data.state.to_list()
+    guessed_states = []
+
+    while len(guessed_states) < 50:
+        answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
+                                        prompt="What's another state's name?").title()
+
+        if answer_state == 'Exit':
+            missing_states = []
+            for state in all_states:
+                if state not in guessed_states:
+                    missing_states.append(state)
+            new_data = pandas.DataFrame(missing_states)
+            new_data.to_csv('./day25/states_to_learn.csv')
+            break
+
+        if answer_state in all_states:
+            t = turtle.Turtle()
+            t.hideturtle()
+            t.penup()
+            state_data = data[data.state == answer_state]
+            t.goto(int(state_data.x), int(state_data.y))
+            t.write(state_data.state.item())
+            guessed_states.append(state_data.state.item())
+
+    turtle.mainloop()
+
+    screen.exitonclick()
